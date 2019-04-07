475ef82e9b27123f84e9bb5f62a5f144aef7e2b5
YukaKadokura <kadokura00000@gmail.com>
2019-04-07 13:59:06 +0900

    first commit

---
+++ okashi/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  okashi
+//
+//  Created by yuka on 2019/04/07.
+//  Copyright © 2019 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var searchText: UISearchBar!
+    
+}
+
